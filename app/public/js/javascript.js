a6ab723034f60a7aedf687edf07e9802c6665723
michael emmons <memmons18@gmail.com>
2020-02-18 22:17:58 -0500

    added individual room data to display dynamically

--- app/public/js/javascript.js
+++ app/public/js/javascript.js
@@ -1,50 +1,62 @@
-// when room-list-page loads:
-// 1) check if there are any room added in the database
-// 2) if there are rooms, get the information about them and place them into an Array
-// 3) loop through the array of rooms populating information as a visual for client side to see
-
-// function for getting information
-// function creating html dynamically
-
-let roomData;
-if (window.location.pathname === "/rooms") {
-  loadData("getrooms", false);
-} else {
-  loadData("getrooms", true);
-}
-
-function loadData(location, needAverage) {
-  $.get("/api/" + location, function(data) {
-    return data;
-  })
-    .done(function(data) {
-      if (data) {
-        roomData = data;
-        console.log(roomData);
-        needAverage ? getAverage(data) : sortData();
-      } else {
-        return;
-      }
-    })
-    .fail(function(err) {
-      console.log(err);
-    });
-}
-
-function sortData() {
-  if (roomData) {
-    for (let i = 0; i < roomData.length; i++) {
-      console.log(roomData);
-      addHTML(roomData[i]);
+// get house data when loading page
+// let something = "../images/living_room.jpg";
+$.get("/house/1", function(data) {})
+  .done(function(data) {
+    if (window.location.pathname === "/rooms") {
+      constructData(data, false);
+    } else {
+      constructData(data, true);
     }
-  } else {
-    console.log("no data");
+  })
+  .fail(function(err) {
+    console.log(err);
+  });
+
+let cleanpoints = 0; /* add room percentage*/
+let rooms; /*Add number of rooms */
+let house_cleanliness; /*average percentage of all the rooms combined*/
+
+function constructData(data, wholeHouse) {
+  rooms = data.length;
+  for (i = 0; i < data.length; i++) {
+    checkRoomStats(data[i], wholeHouse);
+  }
+
+  if (wholeHouse) {
+    let average = Math.round(cleanpoints / rooms);
+
+    createHouseDisplay(data, average, data[0].location, data[0].image);
   }
 }
 
-function addHTML(specificRoom) {
-  console.log(specificRoom.percent);
-  let backgroundcolor = colorCheck(specificRoom.percent);
+// gets the amount of time left to clean and turns it into a percentage
+function checkRoomStats(data, wholeHouse) {
+  let timeframe = parseInt(data.cleaning_time);
+  let now = moment().format("MM/DD/YY");
+  let end = moment(data.end_date);
+  let daysLeft = end.diff(now, "days");
+  daysLeft = parseInt(daysLeft);
+
+  let percentage = createPercentage(daysLeft, data.cleaning_time);
+
+  //this add the percentages of rooms up for house view
+  if (wholeHouse) {
+    cleanpoints += percentage;
+  } else {
+    // create individual room data if on rooms page
+    createHouseDisplay(data, percentage, data.room, data.img);
+  }
+}
+
+// percentage function
+function createPercentage(daysLeft, cleaning_time) {
+  let percent = Math.round((daysLeft / cleaning_time) * 100);
+  return percent;
+}
+
+// dynamically create display of house average data or individual room data
+function createHouseDisplay(data, percent, title, background_image) {
+  let backgroundcolor = colorCheck(percent);
 
   let li = $("<li>");
   let a = $("<a>");
@@ -53,15 +65,17 @@ function addHTML(specificRoom) {
   let percentClean = $("<div>");
   let progressbar = $("<div>");
 
-  h3.text(specificRoom.title);
-  percentClean.text(specificRoom.percent + "%");
+  h3.text(title);
+  percentClean.text(percent + "%");
   percentClean.addClass("percentage-clean");
   progressbar.addClass("progress-bar");
-  progressbar.css("width", specificRoom.percent + "%");
+  progressbar.css("width", percent + "%");
   progressbar.css("background-color", backgroundcolor);
 
+  a.attr("href", "/rooms");
   div.addClass("house-overlay");
   li.attr("id", "house-1");
+  li.css("background-image", "url('" + background_image + "')");
   div.append(h3);
   div.append(percentClean);
   div.append(progressbar);
@@ -81,14 +95,4 @@ function colorCheck(percent) {
   } else {
     return "red";
   }
-}
-
-//get average cleanliness of whole house
-function getAverage(data) {
-  let percentages = 0;
-
-  for (let i = 0; i < data.length; i++) {
-    percentages += data[i].percent;
-  }
-  return percentages / data.length;
 }
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -3,7 +3,10 @@ let connection = require("../config/connection.js");
 module.exports = function(app) {
   app.get("/api/getrooms", function(req, res) {
     res.send([
-      { title: "living room", percent: 50 },
+      {
+        title: "living room",
+        percent: 50
+      },
       { title: "main bedroom", percent: 30 },
       { title: "kitchen", percent: 80 },
       { title: "main bathroom", percent: 49 },
@@ -13,7 +16,7 @@ module.exports = function(app) {
 
   app.get("/house/:userId", function(req, res) {
     let queryRooms =
-      "SELECT house_db.name AS location, rooms_db.name AS room, rooms_db.cleaning_timeframe AS timeframe, rooms_db.last_cleaned AS last_cleaned, rooms_db.end_date AS end_date, rooms_db.cleaning_timeframe AS cleaning_time FROM house_db JOIN rooms_db ON house_db.id = rooms_db.house_id";
+      "SELECT house_db.name AS location, rooms_db.name AS room, rooms_db.cleaning_timeframe AS timeframe, rooms_db.last_cleaned AS last_cleaned, rooms_db.end_date AS end_date, rooms_db.cleaning_timeframe AS cleaning_time, rooms_db.img, house_db.img AS image FROM house_db JOIN rooms_db ON house_db.id = rooms_db.house_id";
 
     connection.query(queryRooms, function(err, result) {
       res.json(result);
